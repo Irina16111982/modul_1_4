cbbf7d3f11044346af6ae0042fa2e65af88d860f
Irina16111982 <i.a.kaparushkina@tmn3.etagi.com>
2024-10-25 10:18:52 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+my_string = ('как вас зовут:')
+print(type(my_string))
+print(len(my_string))
+print(str.upper(my_string))
+print(str.lower(my_string))
+print(my_string.replace(' ', ''))
+print(str(my_string)[0])
+print(my_string[-1])
